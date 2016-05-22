2550b740eb5a6cebf7b62db35cef15f9a377e6c3
awoda <awoda93@gmail.com>
2016-05-22 13:35:45 +0200

    Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer class="footer">
+    <div class="container">
+        Copyright &copy; Adrian Woda 2015-2016r 
+
+        <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Licencja Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a>
+    </div>
+</footer>
+
+</body>
+
+<?php if(isset($conn)){mysqli_close($conn);};?>
+
+</html>
