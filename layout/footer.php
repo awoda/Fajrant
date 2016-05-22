9f1541c4c92db6b82eaa0ffe09d0e887b7c7cc83
awoda <awoda93@gmail.com>
2016-05-22 13:48:49 +0200

    CC licence

--- layout/footer.php
+++ layout/footer.php
@@ -1,6 +1,8 @@
 <footer class="footer">
     <div class="container">
-        <p>Copyright &copy; Adrian Woda 2015-2016r</p>
+        Copyright &copy; Adrian Woda 2015-2016r 
+
+        <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Licencja Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a>
     </div>
 </footer>
 
